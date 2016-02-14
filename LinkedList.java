2306253514f475c4670905c6bf64aaf21fe2441f
Your Name <you@example.com>
2016-02-13 23:32:56 -0700

    Checkpoint.

---
+++ LinkedList.java
@@ -0,0 +1,17 @@
+public class LinkedList {
+
+  public Link firstLink;
+
+  LinkedList(){
+    firstLink = null;
+  }
+
+  public boolean isEmpty(){
+    return (firstLink == null);
+  }
+
+  public void insertFirstLink(String S){
+    Link newLink = new Link
+  }
+
+}
