961cdb320f291d846ff73a64e2c9ee8eafa00101
matt <mattschepers@gmail.com>
2016-02-14 20:01:31 -0700

    Adding/removing from both sides works now. Need to implement iterable.

--- Deque.java
+++ Deque.java
@@ -1,87 +1,121 @@
 
 
-public class Deque{
+public class Deque<Item> {
 
-  private Node headNode;
-  private Node tailNode;
-  private int size;
+private Node headNode;
+private Node tailNode;
+private int size;
 
-  public Deque(){
-    headNode = null;
-    tailNode = null;
-  }
+public Deque(){
+        size = 0;
+        headNode = null;
+        tailNode = null;
+}
 
-  public boolean isEmpty(){
-    return (size == 0);
-  }
+public boolean isEmpty(){
+        return (size == 0);
+}
 
-  public int size(){
-    return size;
-  }
+public int size(){
+        return size;
+}
 
-  public void addFirst(String item){
-    Node newNode = new Node(item);
-    newNode.next = headNode;
-    if(size > 0) headNode.previous = newNode;
-    headNode = newNode;
-    size++;
-  }
+public void addFirst(Item item){
+        size++;
+        //if size is 1 head node is tail node
+        if(size == 1) {
+                headNode = new Node(item);
+                tailNode = headNode;
+        } else {
+                Node newNode = new Node(item);
+                newNode.next = headNode;
+                headNode.previous = newNode;
+                headNode = newNode;
+        }
+}
 
-  public void addLast(String item){
-    Node newNode = new Node(item);
-    newNode.previous = tailNode;
-    if(size > 0) tailNode.next = newNode;
-    tailNode = newNode;
-    size++;
-  }
+public void addLast(Item item){
+        size++;
+        if(size == 1) {
+                tailNode = new Node(item);
+                headNode = tailNode;
+        } else{
+                Node newNode = new Node(item);
+                newNode.previous = tailNode;
+                tailNode.next = newNode;
+                tailNode = newNode;
+        }
+}
 
-  public Node removeFirst(){
-    if(size > 0){
-      Node refNode = headNode;
-      headNode = headNode.next;
-      if(size > 1) headNode.previous = null;
-      size--;
-      return refNode;
-    }
-    else return null;
+public Node removeFirst(){
+        if(size > 0) {
+                Node refNode = headNode;
+                headNode = headNode.next;
+                size--;
+                return refNode;
+        }
+        else {
+                headNode = null;
+                tailNode = null;
+                return null;
+        }
 
-  }
+}
 
-  public Node removeLast(){
-      if(size > 0){
-      Node refNode = tailNode;
-      tailNode = tailNode.previous;
-      if(size > 1) tailNode.next = null;
-      size--;
-      return refNode;
-    } else return null;
-  }
+public Node removeLast(){
+        if(size > 0) {
+                Node refNode = tailNode;
+                tailNode = tailNode.previous;
+                size--;
+                return refNode;
+        }
+        else {
+                headNode = null;
+                tailNode = null;
+                return null;
+        }
+}
 
-  class Node{
-    String data;
-    Node next;
-    Node previous;
-    public Node(String item){
-      data = item;
-    }
+class Node{
+Item data;
+Node next;
+Node previous;
+public Node(Item item){
+        data = item;
+}
 
-  }
+}
 
-  public static void main(String[] args){
-    Deque myDeque = new Deque();
+public static void main(String[] args){
+        Deque<String> myDeque = new Deque<String>();
 
-    System.out.println("Adding args to head.");
-    for(int i = 0; i < args.length; i++){
-      myDeque.addFirst(args[i]);
-    }
+        for(int j = 0; j < 4; j++) {
+                System.out.println("Adding args to head.");
+                for(int i = 0; i < args.length; i++) {
+                        myDeque.addFirst(args[i]+Integer.toString(i));
+                }
 
-    System.out.println("Popping args from tail.");
-    int size = myDeque.size();
-    for(int i = 0; i < size; i++){
-      System.out.println(myDeque.removeLast().data);
-    }
+                System.out.println("Adding args to tail.");
+                for(int i = 0; i < args.length; i++) {
+                        myDeque.addLast(args[i]+Integer.toString(i));
+                }
+
+                if(j%2 == 1) {
+                        System.out.println("Popping args from tail.");
+                        for(int i = 0; myDeque.size > 0; i++) {
+                                System.out.println(myDeque.removeLast().data);
+                        }
+                }
+
+                if(j%2 != 1) {
+                        System.out.println("Popping args from head.");
+                        for(int i = 0; myDeque.size > 0; i++) {
+                                System.out.println(myDeque.removeFirst().data);
+                        }
+                }
+        }
 
 
-  }
+}
 
 }
